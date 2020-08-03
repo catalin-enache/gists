66f8f5fddb8ffb5a628c601b74a86aa780d832bb
Enache Catalin <enachecatalin@Catalins-MBP.lan>
2020-08-03 11:07:29 +0300

    updated merge_sort.js

--- algorithms/sorting/merge_sort.js
+++ algorithms/sorting/merge_sort.js
@@ -48,6 +48,51 @@ function mergeSortLinked(list) {
   mergeLinked(left, right, list);
 }
 
+const mergeSortNonRec1 = (seq) => {
+    const seqLength = seq.length;
+    let _mergeCount = 0;
+    let logN = 0;
+    function merge(left, right) {
+      _mergeCount += (left.length + right.length);
+      if (_mergeCount === seqLength) {
+        logN += 1;
+        _mergeCount = 0;
+      }
+      const list = [];
+      let [i, j] = [0, 0];
+      while (i + j < left.length + right.length) {
+        if (i < left.length && left[i] < right[j] || j === right.length) {
+          list.push(left[i]);
+          i++;
+        } else {
+          list.push(right[j]);
+          j++;
+        }
+      }
+      return list;
+    }
+
+    let mainStack = [];
+    let secStack = [];
+    while (seq.length) {
+        mainStack.push([seq.pop()]);
+    }
+
+    while (mainStack.length) {
+        if (mainStack.length === 1 && !secStack.length) {
+          for (const el of mainStack[0]) seq.push(el);
+          // console.log('logN', logN);
+          return seq;
+        }
+        const left = mainStack.pop();
+        const right = mainStack.length ? mainStack.pop() : [];
+        secStack.push(merge(left, right));
+        if (!mainStack.length) {
+            [mainStack, secStack] = [secStack, mainStack];
+        }
+    }
+}
+
 const list = [4, 2, 8, 5, 1, 0];
 mergeSort(list);
 console.log(list);
